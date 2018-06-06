34d9d02c8e355b91b414fcc84938bfe782d663d4
Eli <elena.terzieva@campus.lmu.de>
2018-06-06 11:42:56 +0200

    adjested the text of the sign up button and the welcoming text

--- app/src/main/java/com/example/ss18/msp/lmu/msp_projectkickoff_ss188/Activities/SettingsActivity.java
+++ app/src/main/java/com/example/ss18/msp/lmu/msp_projectkickoff_ss188/Activities/SettingsActivity.java
@@ -23,6 +23,9 @@ public class SettingsActivity extends AppCompatActivity {
     private static final String PREFS_NAME = "preferences_title";
     private static final String PREF_USER = "preferences_username";
     private EditText enteredUsername;
+    private boolean userNameAlreadyEntered = false;
+    private Button signUpButton;
+    private TextView settingsText;
 
     /**
      * Tag for Logging/Debugging
@@ -36,8 +39,16 @@ public class SettingsActivity extends AppCompatActivity {
         //on the activity menu
         Intent intent = getIntent();
         setContentView(R.layout.settings_fragment);
+        signUpButton = (Button) findViewById(R.id.sign_up);
+        settingsText = (TextView) findViewById(R.id.settings_text);
 
         enteredUsername = (EditText) findViewById(R.id.enter_username);
+        loadPreferences();
+
+        if (userNameAlreadyEntered) {
+            signUpButton.setText("Save");
+            settingsText.setVisibility(View.INVISIBLE);
+        }
     }
 
     /**
@@ -70,6 +81,20 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     /**
+     * Loads the username form the preferences
+     */
+    private void loadPreferences() {
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME,
+                Context.MODE_PRIVATE);
+        // Set username if already existing
+        if(userNameAlreadyEntered = settings.contains(PREF_USER))
+        {
+            LocalDataBase.setUserName(settings.getString(PREF_USER,LocalDataBase.getUserName()));
+            Log.i(TAG,"Load username: " + LocalDataBase.getUserName());
+        }
+    }
+
+    /**
      * Saves the username, that the user picked
      */
     private void setUsername() {
@@ -77,9 +102,14 @@ public class SettingsActivity extends AppCompatActivity {
         if (!enteredUsername.getText().toString().isEmpty()) {
             LocalDataBase.setUserName(enteredUsername.getText().toString());
             savePreferences();
-            Toast.makeText(SettingsActivity.this,
-                    getString(R.string.welcomeUser, LocalDataBase.getUserName()),
-                    Toast.LENGTH_LONG).show();
+            if (userNameAlreadyEntered) {
+                Toast.makeText(SettingsActivity.this, R.string.changedInfo,
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(SettingsActivity.this,
+                        getString(R.string.welcomeUser, LocalDataBase.getUserName()),
+                        Toast.LENGTH_LONG).show();
+            }
             Intent intent = new Intent(SettingsActivity.this,MainActivity.class);
             startActivity(intent);
             } else {
