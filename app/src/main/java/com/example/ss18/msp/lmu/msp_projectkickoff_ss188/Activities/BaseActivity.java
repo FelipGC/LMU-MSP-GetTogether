5bd6a642aefc767e6094b793bf1d1c8b5baf1272
Laureen Wiedemann <wiedemannla@cip.ifi.lmu.de>
2018-06-07 15:44:32 +0200

    Backbutton to navigate up

--- app/src/main/java/com/example/ss18/msp/lmu/msp_projectkickoff_ss188/Activities/BaseActivity.java
+++ app/src/main/java/com/example/ss18/msp/lmu/msp_projectkickoff_ss188/Activities/BaseActivity.java
@@ -13,5 +13,12 @@ public class BaseActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
