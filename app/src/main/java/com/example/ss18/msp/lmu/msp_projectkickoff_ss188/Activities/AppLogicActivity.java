956ee4d28f7f82592c972f9d30d7f6ca6f81e401
Eli <elena.terzieva@campus.lmu.de>
2018-06-12 19:32:50 +0200

    images should appear round

--- app/src/main/java/com/example/ss18/msp/lmu/msp_projectkickoff_ss188/Activities/AppLogicActivity.java
+++ app/src/main/java/com/example/ss18/msp/lmu/msp_projectkickoff_ss188/Activities/AppLogicActivity.java
@@ -167,7 +167,6 @@ public class AppLogicActivity extends BaseActivity {
         Log.i(TAG, "onDestroy() -> terminating nearby connection");
         connectionManager.terminateConnection();
         if (chatFragment != null) {
-            Log.i("Eli6969696", "chat is" + chatFragment);
             chatFragment.clearContent();
         }
         super.onDestroy();
--- app/src/main/java/com/example/ss18/msp/lmu/msp_projectkickoff_ss188/Connection/PayloadReceiver.java
+++ app/src/main/java/com/example/ss18/msp/lmu/msp_projectkickoff_ss188/Connection/PayloadReceiver.java
@@ -182,12 +182,12 @@ public final class PayloadReceiver extends PayloadCallback {
      */
 
     private void onChatMessageReceived(String id, String message) {
-        ChatFragment chat = getAppLogicActivity().getChatFragment();
-        chat.getDataFromEndPoint(id, message);
         //Display notification
         NotificationUtility.displayNotificationChat("Chat message received",
-                String.format("%s has sent you a message...",cM.getEstablishedConnections().get(id).getName()),
+                String.format("%s has sent you a message...",cM.getDiscoveredEndpoints().get(id).getName()),
                 NotificationCompat.PRIORITY_DEFAULT);
+        ChatFragment chat = getAppLogicActivity().getChatFragment();
+        chat.getDataFromEndPoint(id, message);
     }
 
 }
