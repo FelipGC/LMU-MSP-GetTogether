dfab3acdac488abe9b668d812fe121f8c03ee855
Eli <elena.terzieva@campus.lmu.de>
2018-06-22 20:13:19 +0200

    images saves as uri and bitmap so it doesn't lag

--- app/src/main/java/com/example/ss18/msp/lmu/msp_projectkickoff_ss188/DataBase/AppPreferences.java
+++ app/src/main/java/com/example/ss18/msp/lmu/msp_projectkickoff_ss188/DataBase/AppPreferences.java
@@ -2,8 +2,14 @@ package com.example.ss18.msp.lmu.msp_projectkickoff_ss188.DataBase;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.preference.PreferenceManager;
+import android.util.Base64;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
 
 public class AppPreferences {
 
@@ -22,6 +28,7 @@ public class AppPreferences {
 
     static final String PREF_USER = "preferences_username";
     static final String PREF_IMAGE = "preferences_image";
+    static final String PREF_BITMAP = "preferences_bitmap";
 
     public String getUsername(){
         String username = preferences.getString(PREF_USER, null);
@@ -37,6 +44,22 @@ public class AppPreferences {
         }
     }
 
+    public Bitmap getUserImageBitmap(){
+        String image = preferences.getString(PREF_BITMAP, null);
+        if(image == null) return null;
+        else{
+            try{
+                byte [] encodeByte = Base64.decode(image,Base64.DEFAULT);
+                Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+                Log.i("Eli696969", "get image is " + encodeByte + " and get string is : " + image);
+                return bitmap;
+            }catch(Exception e){
+                e.getMessage();
+                return null;
+            }
+        }
+    }
+
     public boolean setUsername(String username){
         boolean success = saveStringPreference(PREF_USER, username);
         if(success) LocalDataBase.setUserName(username); //TODO dirty!
@@ -46,6 +69,17 @@ public class AppPreferences {
     public boolean setUserImage(String uri){
         boolean success = saveStringPreference(PREF_IMAGE, uri);
         if(success) LocalDataBase.setProfilePictureUri(Uri.parse(uri)); //TODO dirty!
+        return success;
+    }
+
+    public boolean setUserImageBitmap(Bitmap bitmap) {
+        Log.i("Eli696969", "image is " + bitmap + " and string is : " + bitmap.toString());
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100, baos);
+        byte [] b=baos.toByteArray();
+        String image = Base64.encodeToString(b, Base64.DEFAULT);
+        boolean success = saveStringPreference(PREF_BITMAP, image);
+        if(success) LocalDataBase.setProfilePictureBitmap(bitmap);
         return success;
     }
 
